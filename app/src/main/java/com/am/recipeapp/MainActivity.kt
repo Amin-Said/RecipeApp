d8d9451e32665523872d4a34f638cbe7dfae6557
Amin <aminsaid_2011@hotmail.com>
2021-04-01 21:41:27 +0200

    Initial commit

---
+++ app/src/main/java/com/am/recipeapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.am.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       // image path // https://spoonacular.com/recipeImages/beef-tenderloin-sandwiches-with-herb-mayonnaise-2-14047.jpg
+       // 9b6f5b56f1msh06d5809f56fa955p1c4991jsnc4d1a3de0ead
+        // https://spoonacular.com/recipeImages/579247-556x370.jpg
+        // key 9d2e702ace7d447a8060f00429795e06
+    }
+}
